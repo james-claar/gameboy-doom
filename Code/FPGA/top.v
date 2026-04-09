9ba9bc585176aafda0d392ea9e95ffd9f759359a
James Claar <jamesdclaar@gmail.com>
2026-04-08 23:48:42 -0500

    Added other people's repos

--- Code/FPGA/top.v
+++ Code/FPGA/top.v
@@ -1,7 +1,26 @@
 
 
 module top (
-    ports
+    // FPGA
+    input fpgaClk,
+    input fpgaReset,
+
+    // Cartridge pins
+    input gbClk,
+    input gbWriteInverted,
+    input gbReadInverted,
+    input gbChipSelectInverted,
+    input [15:0] gbAddress,
+
+    inout [7:0] gbData,
+    inout gbResetInverted
 );
-    
+
+wire gbWrite = ~gbWriteInverted;
+wire gbRead = ~gbReadInverted;
+wire gbChipSelect = ~gbChipSelectInverted;
+wire gbResetReadOnly = ~gbResetInverted;
+
+
+
 endmodule
